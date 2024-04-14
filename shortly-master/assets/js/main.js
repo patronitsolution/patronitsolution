350cfdc89fabc10f38fe34c6c00b63cfba3cf085
patronitsolution <166983283+patronitsolution@users.noreply.github.com>
2024-04-14 15:09:04 +0530

    Delete shortly-master directory
    ghjkl

--- shortly-master/assets/js/main.js
+++
@@ -1,7 +0,0 @@
-
-window.onload = function() {
-    var preloader = document.getElementsByClassName('preloader')[0];
-    setTimeout(function(){
-        preloader.style.display = 'none';
-    }, 500);
-};
