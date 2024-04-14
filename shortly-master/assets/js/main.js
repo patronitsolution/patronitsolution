f62ab72a1f086f506bd8d43900147b9f8ced6ba7
patronitsolution <166983283+patronitsolution@users.noreply.github.com>
2024-04-14 15:04:30 +0530

    Add files via upload
    adding coming soon web

---
+++ shortly-master/assets/js/main.js
@@ -0,0 +1,7 @@
+
+window.onload = function() {
+    var preloader = document.getElementsByClassName('preloader')[0];
+    setTimeout(function(){
+        preloader.style.display = 'none';
+    }, 500);
+};
